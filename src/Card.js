edf8bd71fe73c1e335d436b6f23eebcd975f8172
satyamraj2358 <satyamraj2358@gmail.com>
2023-03-28 21:56:39 +0530

    created multiple cards with data from robots.js as props

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ name, id, email }) => {
   return (
-    //this is jsx, and needs react to be imported
     <div className="bg-light-green dib br3 pa3 ma2 grow">
-      <img src="https://robohash.org/test?200x200" alt="robots" />
+      <img src={`https://robohash.org/${id}?200x200`} alt="robots" />
       <div>
-        <h2>Jane Doe</h2>
-        <p>janedoe@email.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
@@ -16,4 +15,7 @@ const Card = () => {
 // module.exports = { }
 export default Card;
 
+//this is jsx, and needs react to be imported
 // https://robohash.org/random_value => return a random image of robo
+
+//we destrutre the arguments within . { name, id, email }
